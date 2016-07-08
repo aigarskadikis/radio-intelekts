9d0ed901d3e591138317e63a07736f5cb6842d42
catonrug <aigars.kadikis@gmail.com>
2016-07-08 15:33:43 +0300

    next

--- install-as-service.sh
+++ install-as-service.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+#copy main script to users [pi] home directory
 cp intelekts.sh /home/pi
+
+#make main script executable
 chmod 755 /home/pi/intelekts.sh
 
+#install service, so the radio will start to stream at system boot
 cat > /etc/init.d/radio << EOF
 #!/bin/sh
 case "\$1" in
@@ -13,6 +17,16 @@ echo "Radio started"
 stop)
 mpc stop
 echo "Radio stopped"
+sleep 1
+;;
+restart)
+mpc stop
+echo "Radio stopped"
+sleep 1
+> /var/log/mpd/mpd.log
+sleep 1
+/home/pi/intelekts.sh &
+echo "Radio restarted"
 ;;
 *)
 echo "Use start or stop parameter"
@@ -21,5 +35,11 @@ exit 1
 esac
 EOF
 
+#set service file executable
 chmod 755 /etc/init.d/radio
+
+#re-read all service situation
 update-rc.d radio defaults
+
+#shedule to restart radio two times in 24 hours
+echo "55 14,3 * * * root /etc/init.d/radio restart" >> /etc/crontab
